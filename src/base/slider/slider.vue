d3389e501bff60077346e1a930f5800a28b27e16
wangjianguo007 <18734203672@163.com>
2017-11-25 17:37:55 +0800

    轮播结束，better-scroll版本降级

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -8,7 +8,7 @@
      </slot>
    </div>
    <div class="dots">
-     
+     <span class="dot" v-for="(item,index) in dots" :class="{active:currentPageIndex===index}"></span>
    </div>
   </div>
 </template>
@@ -32,16 +32,34 @@ import {addClass} from "common/js/dom"
         default:4000
       }
     },
+    data(){
+      return {
+        dots:[],
+        currentPageIndex: 0//默认点的下标是0
+      }
+    },
     mounted(){
       /*$nextTick也可以归定时间加载*/
       setTimeout(()=>{
         this._setSliderWidth()
+        this._initDots()
         this._initSlider()
+        if(this.autoPlay){
+          this._play()
+        }
       },20)
+      /*解决窗口适配的问题,解决后变成图片随浏览器适配,resize是better-scroll的事件*/
+      window.addEventListener("resize",()=>{
+        if(!this.slider){
+          return
+        }
+        this._setSliderWidth(true)
+        this.slider.refresh()//刷新better-scroll的方法
+      })
     },
     methods:{
       /*获取每个图的宽度的方法*/
-      _setSliderWidth(){
+      _setSliderWidth(isResize){
         this.children=this.$refs.sliderGroup.children
         /*width是全部图片的宽度*/
         let width=0
@@ -55,7 +73,8 @@ import {addClass} from "common/js/dom"
           child.style.width=sliderWidth+'px'
           width+=sliderWidth
         }
-        if(this.loop){
+        /*有了改变就停止了*/
+        if(this.loop&&!isResize){
           width+=2*sliderWidth
         }
         this.$refs.sliderGroup.style.width=width+'px'
@@ -69,9 +88,37 @@ import {addClass} from "common/js/dom"
           snap: true, /*该属性是给 slider 组件使用的，普通的列表滚动不需要配置*/
           snapLoop:this.loop,/*循环*/
           snapThreshold: 0.3, /*用手指滑动时页面可切换的阈值，大于这个阈值可以滑动的下一页*/
-          snapSpeed:400,/*滑动的速度*/
-          click:true
+          snapSpeed:400/*滑动的速度*/
+          /*click:true去掉了否则手机无法点击（反正是手机端）*/
         })
+        /*每次在滚动完毕后，会有一个scrollEND事件，getCurrentPage()获得当前页面，better-scroll方法*/
+        this.slider.on("scrollEnd",()=>{
+          let pageIndex=this.slider.getCurrentPage().pageX//第几个子元素
+          /*如果循环，他就减少一个图片拼接是7个*/
+          if(this.loop){
+            pageIndex-=1
+          }
+          this.currentPageIndex=pageIndex
+          /*解决轮播滚动到第二张停顿的问题*/
+          if(this.autoPlay){
+            clearTimeout(this.timer)
+            this._play()
+          }
+        })
+      },
+      /*轮播上的点*/
+      _initDots(){
+        this.dots=new Array(this.children.length)
+      },
+      /*自动轮播*/
+      _play(){
+        let pageIndex=this.currentPageIndex+1
+        if(this.loop){
+          pageIndex+=1
+        }
+        this.timer=setTimeout(()=>{
+          this.slider.goToPage(pageIndex,0,400)//这是better-scroll的方法最后时间和上边滑动时间一样
+        },this.interval)
       }
     }
   }
