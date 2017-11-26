cc27ee2297e92c1815f30a4dbb2ef8af36fbda9f
wangjianguo007 <18734203672@163.com>
2017-11-26 21:01:51 +0800

    8热门歌单文本渲染和scroll组件及优化

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,65 @@
+<!-- 建立scroll组件 -->
+<template>
+  <div ref="wapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+  export default{
+    props:{
+      probeType:{//下拉的速度，就是截流
+        type:Number,
+        default:1
+      },
+      click:{
+        type:Boolean,
+        default:true
+      },
+      data:{//数据改变，为了refresh()
+        type:Array,
+        default:null
+      }
+    },
+    mounted(){
+      /*浏览器是17ms*/
+      setTimeout(()=>{
+        this._initScroll()
+      },20)
+    },
+    methods:{
+      _initScroll(){
+        if(!this.$refs.wapper){//没有至就是data为null
+          return
+        }
+        this.scroll=new BScroll(this.$refs.wapper,{
+          probeType:this.probeType,
+          click:this.click
+        })
+      },
+      /*以下就是better-scroll代理，就是将方法套用了下*/
+      enable(){//允许
+        this.scroll && this.scroll.enable()
+      },
+      disable(){//禁止
+        this.scroll && this.scroll.disable()
+      },
+      refresh(){
+        this.scroll && this.scroll.refresh()
+      }
+    },
+    /*监听data变化，如果有变化在初始化的时候刷新*/
+    watch:{
+      data(){
+        setTimeout(()=>{
+          this.refresh()
+        },20)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="recommend">
-    <div class="recommend-content">
+    <!--我们在打开页码的时候，开始渲染是没有高度的，没有数据的，而scroll实在mounted的方法下，所以就滚动不了
+      所以我们可以给他加data方法让他有数据，数据不为null，discList在轮播后加载，所以会撑开内容的高度
+      所以就可以实现向上滚动了，还有就是加以一个div将轮播和内容全部抱在一起，父用子元素方法时候可以使用
+      v-bing的方法-->
+    <Scroll ref="scroll" class="recommend-content" :data="discList">
+      <div>
         <div class="slider-wrapper" v-if="recommends.length">
           <Slider>
             <div v-for="item in recommends">
               <a :href="item.linkUrl">
-                <img :src="item.picUrl" alt="">
+                <img :src="item.picUrl" @load="loadImgage">
               </a>
             </div>
           </Slider>
@@ -13,10 +18,20 @@
         <div class="recommend-list">
           <h1 class="list-title">热门歌单推荐</h1>
           <ul>
+            <li class="item" v-for="item in discList">
+              <div class="icon">
+                <img :src="item.imgurl" width="60" height="60">
+              </div>
+              <!--v-html就是转易，防止有代码-->
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
-    </div>
+    </Scroll>
   </div>
 </template>
 
@@ -24,13 +39,18 @@
 import Slider from "base/slider/slider"
 import {getRecomment,getDiscList} from "api/recommend"
 import {ERR_OK} from "api/config"
+import Scroll from "base/scroll/scroll"
   export default{
     data(){
       return {
-        recommends:[]
+        recommends:[],
+        discList:[]
       }
     },
   	created(){
+     /* setTimeout(()=>{
+        this._getRecomment()
+      },5000)*///测试滚动
   		this._getRecomment()//轮播数据
       this._getDiscList()//歌单数据
   	},
@@ -47,13 +67,22 @@ import {ERR_OK} from "api/config"
       _getDiscList(){
         getDiscList().then((res)=>{
           if(res.code===ERR_OK){
-            console.log(res.data.list)
+            //console.log(res.data.list)
+            this.discList=res.data.list
           }
         })
+      },
+      /*有一张图片加载好了就会重新异步刷新页面，确定滚动的高度,否则下边的看不到，这是优化*/
+      loadImgage(){
+        if(!this.checkloaded){
+          this.$refs.scroll.refresh()
+          this.checkloaded=true
+        }
       }
   	},
     components:{
-      Slider
+      Slider,
+      Scroll
     }
   }
 </script>
@@ -82,6 +111,7 @@ import {ERR_OK} from "api/config"
         .item
           display: flex
           box-sizing: border-box
+          //这是flex横向居中的方法
           align-items: center
           padding: 0 20px 20px 20px
           .icon
@@ -90,6 +120,7 @@ import {ERR_OK} from "api/config"
             padding-right: 20px
           .text
             display: flex
+            //纵向排列居中
             flex-direction: column
             justify-content: center
             flex: 1
