3a783a4503e412c7e265aa52d38d25e0fc4abba4
wangjianguo007 <18734203672@163.com>
2017-11-28 22:22:15 +0800

    10歌手数据的接口

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,14 +1,40 @@
 <template>
   <div class="singer">
-    singer
+   	
   </div>
 </template>
 
-<script type="text/ecmascript-6">
-  export default {}
+<script>
+import {getSingerList} from "api/singer"
+import {ERR_OK} from "api/config"
+  export default {
+  	data(){
+  		return {
+  			singers:[]
+  		}
+  	},
+  	created(){
+  		/*歌手的列表*/
+  		this._getSingerList()
+  	},
+  	methods:{
+  		_getSingerList(){//渲染到页面到数据
+  			getSingerList().then((res)=>{
+  				if(res.code===ERR_OK){
+  					this.singers=res.data.list
+  					//console.log(this.singers)
+  				}
+  			})
+  		}
+  	}
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
-  
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
