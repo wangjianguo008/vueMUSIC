cc785be34d46d06dbdc1c2a8272b22a40bc21722
wangjianguo007 <18734203672@163.com>
2017-12-08 22:55:35 +0800

    21歌手歌单详情向上拉和向下拉动画

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -7,8 +7,10 @@
     <div class="bg-image" :style="Bgstyle" ref="bgImage">
       <div class="filter"></div>
     </div>
+    <!--这是为滚动坐了一个滚动层-->
+    <div class="bg-layer" ref="layer"></div>
     <!--:data="songs"为了获取内容和高度-->
-    <Scroll :data="songs" class="list" ref="list">
+    <Scroll :data="songs" class="list" ref="list" :probeType="probeType" :listenScroll="listenScroll" @scroll="scroll">
       <div class="song-list-wrapper">
         <songList :songs="songs"></songList>
       </div>
@@ -19,6 +21,7 @@
 <script>
 import Scroll from 'base/scroll/scroll'
 import songList from 'base/song-list/song-list'
+const RESERVED_HEIGHT=40
   export default{
     props:{
       /*从父元素传过来的数据*/
@@ -41,13 +44,52 @@ import songList from 'base/song-list/song-list'
         return `background-image:url(${this.bgImage})`
       }
     },
+    data(){
+      return {
+        scrollY:0
+      }
+    },
+    created(){
+      /*为了做监听的滚动*/
+      this.probeType=3,
+      this.listenScroll = true
+    },
     mounted(){
+      /*在生命周期中变量可以使用this.xxx*/
+      this.imageHeight=this.$refs.bgImage.clientHeight
+      /*设置一个到图片的滚动值，向上就是负负的正*/
+      this.minTransalteY=-this.imageHeight+RESERVED_HEIGHT
       /*在挂载的时候可以看到图片和列表，这个样式是width：100%和padding-top：70%,这个$el是dom用于样式*/
-      this.$refs.list.$el.style.top=`${this.$refs.bgImage.clientHeight}px`
+      this.$refs.list.$el.style.top=`${this.imageHeight}px`
     },
     components:{
       Scroll,
       songList
+    },
+    methods:{
+      /*做监听scroll滚动了多少，赋值给this.scrollY*/
+      scroll(pos){
+        this.scrollY=pos.y
+      }
+    },
+    watch:{
+      scrollY(newY){
+        /*当滚到一定的时候zIndex让image出现，高度改变*/
+        let zIndex=0
+        /*比较滚动和滚动到图片的两个值的大小 newY按top算的*/
+        let translateY=Math.max(this.minTransalteY,newY)
+        /*将scroll监听到的高度给滚动层赋值*/
+        this.$refs.layer.style['transform']=`translate3d(0,${translateY}px,0)`
+        if(newY<this.minTransalteY){
+          zIndex=10
+          this.$refs.bgImage.style.paddingTop=0
+          this.$refs.bgImage.style.height=`${RESERVED_HEIGHT}px`
+        }else{
+          this.$refs.bgImage.style.paddingTop='70%'
+          this.$refs.bgImage.style.height=0
+        }
+        this.$refs.bgImage.style.zIndex=zIndex
+      }
     }
   }
 </script>
