894e70d53880621ae61b42436b638e23e901cdae
wangjianguo007 <18734203672@163.com>
2017-12-09 17:05:24 +0800

    22歌手详情放大前缀封装随机播放返回加载

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -24,3 +24,34 @@ export function getData(el,name,val){
 		return el.getAttribute(name)
 	}
 }
+
+/*浏览其器前缀的问题，统一的管理全加*/
+let elementStyle=document.createElement('div').style
+/*创建快速的方法*/
+let vendor = (() => {
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+  for (let key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key
+    }
+  }
+  return false
+})()
+export function prefixStyle(style) {
+  if (vendor === false) {
+    return false
+  }
+
+  if (vendor === 'standard') {
+    return style
+  }
+/*类似vendor就是key style.charAt(0).toUpperCase()就是T style.substr(1)就是ransfrom 最后拼接*/
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
+
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="Bgstyle" ref="bgImage">
-      <div class="filter"></div>
+      <div class="play-wrapper">
+        <div ref="playBtn" class="play" v-show="songs.length>0">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+      <div class="filter" ref="filter"></div>
     </div>
     <!--这是为滚动坐了一个滚动层-->
     <div class="bg-layer" ref="layer"></div>
@@ -14,6 +20,9 @@
       <div class="song-list-wrapper">
         <songList :songs="songs"></songList>
       </div>
+      <div v-show="!songs.length" class="loading-container">
+        <loading></loading>
+      </div>
     </Scroll>
   </div>
 </template>
@@ -21,6 +30,10 @@
 <script>
 import Scroll from 'base/scroll/scroll'
 import songList from 'base/song-list/song-list'
+import {prefixStyle} from 'common/js/dom'
+import loading from 'base/loading/loading'
+const transform=prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 const RESERVED_HEIGHT=40
   export default{
     props:{
@@ -64,31 +77,53 @@ const RESERVED_HEIGHT=40
     },
     components:{
       Scroll,
-      songList
+      songList,
+      loading
     },
     methods:{
       /*做监听scroll滚动了多少，赋值给this.scrollY*/
       scroll(pos){
         this.scrollY=pos.y
+      },
+      back(){
+        /*利用路由返回*/
+        this.$router.back()
       }
     },
     watch:{
       scrollY(newY){
-        /*当滚到一定的时候zIndex让image出现，高度改变*/
+        /*当滚到一定的时候zIndex让image出现，高度改变 向上推*/
         let zIndex=0
+        /*当滚动到top为0的时候，开始scale缩放*/
+        let scale=1
+        /*向上滚动时候做高斯模糊*/
+        let blur=0
+        /*缩放的比例计算就是根据下拉距离，来缩放图片大小 abs是绝对值*/
+        const percent=Math.abs(newY/this.minTransalteY)
         /*比较滚动和滚动到图片的两个值的大小 newY按top算的*/
         let translateY=Math.max(this.minTransalteY,newY)
+        if(newY>0){
+          scale=1+percent
+          zIndex=10
+        }else{
+          blur=Math.min(20*percent,20)
+        }
         /*将scroll监听到的高度给滚动层赋值*/
-        this.$refs.layer.style['transform']=`translate3d(0,${translateY}px,0)`
+        this.$refs.layer.style[transform]=`translate3d(0,${translateY}px,0)`
+        /*模糊效果在filter，backdrop-filter对图片背景的模糊*/
+        this.$refs.filter.style[backdrop]=`blur(${blur}px)`
         if(newY<this.minTransalteY){
           zIndex=10
           this.$refs.bgImage.style.paddingTop=0
           this.$refs.bgImage.style.height=`${RESERVED_HEIGHT}px`
+          this.$refs.playBtn.style.display='none'
         }else{
           this.$refs.bgImage.style.paddingTop='70%'
           this.$refs.bgImage.style.height=0
+          this.$refs.playBtn.style.display=''
         }
         this.$refs.bgImage.style.zIndex=zIndex
+        this.$refs.bgImage.style[transform]=`scale(${scale})`
       }
     }
   }
